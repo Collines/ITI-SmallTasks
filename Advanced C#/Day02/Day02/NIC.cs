d96ec36424c52605dad8c313b3213a9d06f93cab
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-18 15:16:03 +0200

    Adding Day02 Advanced C# Task solution

---
+++ Advanced C#/Day02/Day02/NIC.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    // 
+    // NIC card must have these data: Manufacture, MAC Address, Type [Ethernet or token ring – use Enumeration here]…
+    //
+    enum Type:byte
+    {
+        Ethernet,
+        TokenRing
+    }
+    internal class NIC
+    {
+        private StringBuilder Manfacture;
+        private StringBuilder MACAddress;
+        private Type ConnectionType;
+
+        private NIC() {
+            Manfacture = new StringBuilder("Samsung");
+            MACAddress = new StringBuilder("00-B0-D0-63-C2-26");
+            ConnectionType = Type.Ethernet;
+        }
+        public static NIC instance { get; } = new NIC();
+        public void connectionData()
+        {
+            Console.WriteLine($"{ConnectionType} {Manfacture} Connection with {MACAddress} Address is connected");
+        }
+    }
+
+    class X
+    {
+        public int x; // value type
+        public Boolean y; // value type
+        public int[] arr; // ref type
+        public X(int _x, Boolean _y, int[] arr)
+        {
+            x = _x; y = _y;
+            this.arr = arr;
+        }
+
+        public void clone(X V)
+        {
+            x=V.x; y = V.y ; arr=V.arr;
+        }
+
+        public override string ToString()
+        {
+            string str="";
+            foreach (int e in arr)
+                str += $" {e}";
+            return $"X: {x} - Y:{y} - Arr:{str}";
+        }
+  
+    }
+}
---
+++ Advanced C#/Day02/Day02/Program.cs
@@ -0,0 +1,56 @@
+﻿using Day02;
+
+
+//int size;
+//Console.WriteLine("Enter Array size: ");
+//while (!int.TryParse(Console.ReadLine(), out size))
+//{
+
+
+//    Console.WriteLine("Enter a valid number");
+//}
+//int[] arr = new int[size];
+//for (int i = 0; i < size; i++)
+//{
+//    int el;
+//    Console.WriteLine($"Enter element({i + 1}):");
+//    while (!int.TryParse(Console.ReadLine(), out el))
+//    {
+//        Console.WriteLine("Enter a valid number");
+//    }
+//    arr[i] = el;
+//}
+
+//int element = 0, index1 = 0, index2 = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    for (int j = 1; j < arr.Length; j++)
+//    {
+//        if (arr[i] == arr[j])
+//        {
+//            if (index2 - index1 < j - i)
+//            {
+//                index1 = i;
+//                index2 = j;
+//                element = arr[i];
+//            }
+//        }
+//    }
+//}
+//Console.WriteLine($"the difference between {element} and the last {element} is {index2 - index1 - 1} cells");
+
+//NIC x = NIC.instance;
+//NIC y = NIC.instance;
+//Console.WriteLine(x.GetHashCode());
+//Console.WriteLine(y.GetHashCode());
+//x.connectionData();
+//y.connectionData();
+X x = new X(10, true, new int[] {1,2,3,4});
+X y = new X(20, false, new int[] { 10, 20, 30, 40 });
+Console.WriteLine(x.ToString());
+Console.WriteLine(y.ToString());
+y.clone(x);
+x.x = 19999;
+x.arr[0] = 200;
+Console.WriteLine(x.ToString());
+Console.WriteLine(y.ToString());
