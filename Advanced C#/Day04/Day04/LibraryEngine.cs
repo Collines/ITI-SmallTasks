d145bf2f897c134896060341472529dcd171aeef
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-18 15:18:58 +0200

    Adding Day04 Advanced C# Task solution

---
+++ Advanced C#/Day04/Day04/LibraryEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    public delegate string bookFunctionsDelDT(Book b); // User defined delegate
+    internal static class LibraryEngine
+    {
+        //public static void ProcessBooks(List<Book> bList, bookFunctionsDelDT fPtr)
+        //{
+        //    foreach (Book B in bList)
+        //    {
+        //        Console.WriteLine(fPtr(B));
+        //    }
+        //}
+
+        public static void ProcessBooks(List<Book> bList, Func<Book,string> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+
+    }
+}
---
+++ Advanced C#/Day04/Day04/Program.cs
@@ -0,0 +1,22 @@
+﻿using Day04;
+List<Book> BookLists = new() { new("HTC", "How to code", new string[] { "MoSalah", "Ahmed" }, new DateTime(1997,6,3), 10000),
+                               new("HTD", "How to debug", new string[] { "Abdulrazek", "Khaled" }, new DateTime(1999,8,3), 2000),
+                               new("HTMF", "How to make friends", new string[] { "Ayman", "Tamer" }, new DateTime(1995,6,3), 5000),
+};
+
+
+// anonymous method to get ISBN
+LibraryEngine.ProcessBooks(BookLists, delegate (Book x) { return x.ISBN; });
+
+// lambda expression to get Publication dates 
+LibraryEngine.ProcessBooks(BookLists, x => x.PublicationDate.ToString());
+
+// user define delegate
+LibraryEngine.ProcessBooks(BookLists, BookFunctions.GetTitle);
+Console.WriteLine("-----------------------------------------");
+LibraryEngine.ProcessBooks(BookLists, BookFunctions.GetAuthors);
+Console.WriteLine("-----------------------------------------");
+
+LibraryEngine.ProcessBooks(BookLists, BookFunctions.GetPrice);
+Console.WriteLine("-----------------------------------------");
+LibraryEngine.ProcessBooks(BookLists, BookFunctions.GetAuthors);
