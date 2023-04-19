5c5120e0ec86e227412fa210690980337c341faa
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-19 16:50:53 +0200

    Adding Day10 Advanced C# Task solution

---
+++ Advanced C#/Day10/DataView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Day10.Entities;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Day10
+{
+    /// <summary>
+    /// Interaction logic for DataView.xaml
+    /// </summary>
+    public partial class DataView : Window
+    {
+        pubsContext Context;
+        public DataView()
+        {
+            Context = new pubsContext();
+            InitializeComponent();
+            Closing += (sender, e) => Context.Dispose();
+            Load();
+        }
+
+        private void Load()
+        {
+            if (!listView.Items.IsEmpty)
+                listView.ItemsSource = null;
+            Context.titles.Load();
+            Context.publishers.Load();
+            listView.ItemsSource = Context.titles.Local.ToBindingList();
+            listView.DisplayMemberPath = "title1";
+            listView.SelectedValuePath = "title_id";
+            listView.SelectionChanged += (sender, e) => StackPanelCustom.DataContext = listView.SelectedItem;
+            PrdID.SetBinding(ContentProperty, new Binding("title_id"));
+            PrdName.SetBinding(TextBox.TextProperty, new Binding("title1"));
+            PrdUnitPrice.SetBinding(TextBox.TextProperty, new Binding("price"));
+            CmbBox.ItemsSource = Context.publishers.Local.ToBindingList();
+            CmbBox.SelectedValuePath = "pub_id";
+            CmbBox.DisplayMemberPath = "pub_name";
+            CmbBox.SetBinding(ComboBox.SelectedValueProperty, new Binding("pub_id"));
+        }
+
+        private void Reload(object sender, RoutedEventArgs e)
+        {
+            Context.Dispose();
+            Context = new pubsContext();
+            Load();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            var Result = MessageBox.Show("Do you want to save?", "Confirmation", MessageBoxButton.YesNo);
+            if (Result == MessageBoxResult.Yes)
+            {
+                Context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Advanced C#/Day10/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Day10.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        pubsContext Context;
+        public MainWindow()
+        {
+            Context = new pubsContext();
+            InitializeComponent();
+            Closing += (sender, e) => Context.Dispose();
+        }
+
+        private void DataGridLoaded(object sender, RoutedEventArgs e)
+        {
+            DataGridComboBoxColumn ComboCol = new();
+            ComboCol.SelectedValueBinding = new Binding("pub_id");
+            Context.titles.Load(); // Select * from Titles, result stored in local dbSet
+            Context.publishers.Load();
+            ComboCol.ItemsSource = Context.publishers.Local.ToBindingList();
+            ComboCol.Header = "Publisher";
+            ComboCol.DisplayMemberPath = "pub_name";
+            ComboCol.SelectedValuePath = "pub_id";
+            //
+            dataGrid.ItemsSource = Context.titles.Local.ToBindingList();
+            dataGrid.Columns.ElementAt(3).IsReadOnly = true;
+            dataGrid.Columns.ElementAt(3).Header = "Publisher";
+            dataGrid.Columns.ElementAt(8).Width= 100;
+            dataGrid.Columns.ElementAt(1).Width= 100;
+            dataGrid.Columns.RemoveAt(10);
+            dataGrid.Columns.Add(ComboCol);
+        }
+
+        private void Reload(object sender, RoutedEventArgs e)
+        {
+            var Result = MessageBox.Show("Do you want to reload?", "Confirmation", MessageBoxButton.YesNo);
+            if(Result == MessageBoxResult.Yes)
+            {
+                Context = new pubsContext();
+                dataGrid.ItemsSource = null;
+                dataGrid.Columns.Clear();
+                dataGrid.Items.Clear();
+                DataGridLoaded(sender, e);
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            var Result = MessageBox.Show("Do you want to save?", "Confirmation", MessageBoxButton.YesNo);
+            if (Result == MessageBoxResult.Yes)
+            {
+                Context.SaveChanges();
+            }
+        }
+    }
+}
