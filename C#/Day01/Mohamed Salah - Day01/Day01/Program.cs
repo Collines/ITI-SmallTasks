235de17afaaf47bf42b550869977047acc8633bd
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-22 11:18:17 +0200

    Added C# Day01 Task solution

---
+++ C#/Day01/Mohamed Salah - Day01/Day01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter two numbers\n");
+Console.WriteLine("Enter First Number");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("{0} + {1} = {2}",num1,num2,num1+num2);
+Console.WriteLine("{0} - {1} = {2}",num1, num2, num1 - num2);
+Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 *num2);
+Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
