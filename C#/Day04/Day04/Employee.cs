d84e68f003c9a552c4d52074bbb59c422150d47a
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-22 11:21:18 +0200

    Added C# Day04 Task solution

---
+++ C#/Day04/Day04/Employee.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    public enum Gender
+    {
+        Male, Female
+    }
+
+    public enum SecurityLevel : byte
+    {
+        Guest, Developer, Secretary, DBA
+    }
+
+    public struct Date
+    {
+        int Year;
+        int Month;
+        int Day;
+        public Date(int day, int month, int year)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        public int GetYear()
+        {
+            return Year;
+        }
+        public int GetMonth()
+        {
+            return Month;
+        }
+        public int GetDay()
+        {
+            return Day;
+        }
+    }
+    struct Employee
+    {
+        private int Id;
+        private string Name;
+        private SecurityLevel Security;
+        private float Salary;
+        private Date HiringDate;
+        private Gender Gen;
+
+        public Employee(int id, string name, SecurityLevel security, float salary, Date hiringDate, Gender gen)
+        {
+            Id = id;
+            Name = name;
+            Security = security;
+            Salary = salary;
+            HiringDate = hiringDate;
+            Gen = gen;
+        }
+
+        // Getters
+        public int GetId()
+        {
+            return Id;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public SecurityLevel GetSecurityLevel()
+        {
+            return Security;
+        }
+        public float GetSalary()
+        {
+            return Salary;
+        }
+        public Date GetHiringDate()
+        {
+            return HiringDate;
+        }
+        public Gender GetGender()
+        {
+            return Gen;
+        }
+
+        //Setters
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public void SetSecurity(SecurityLevel security)
+        {
+            Security = security;
+        }
+        public void SetSalary(float salary)
+        {
+            Salary = salary;
+        }
+        public void setHiringDate(Date hiringDate)
+        {
+            HiringDate = hiringDate;
+        }
+        public void SetGender(Gender gender)
+        {
+            Gen = gender;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Id} - {Name} - {Security} - {Salary} - {HiringDate.GetDay()}/{HiringDate.GetMonth()}/{HiringDate.GetYear()} - {Gen}");
+        }
+
+    }
+}
---
+++ C#/Day04/Day04/Program.cs
@@ -0,0 +1,50 @@
+﻿using Day04;
+
+Employee[] Emps= new Employee[1];
+Console.WriteLine($"Enter {Emps.Length} Employees data");
+for (int i = 0; i < Emps.Length; i++)
+{
+    Console.WriteLine("Enter Employee's ID");
+    int id;
+    do
+    {
+        id = int.Parse(Console.ReadLine());
+        if (id <= 0)
+            Console.WriteLine("Enter a valid ID");
+    }
+    while (id <= 0);
+    Console.WriteLine("Enter Employee's Name");
+    string name = Console.ReadLine();
+    Console.WriteLine("Enter Employee's Security Level (Guest-Developer-Secretary-DBA)");
+    SecurityLevel security = (SecurityLevel)Enum.Parse(typeof(SecurityLevel), Console.ReadLine());
+    Console.WriteLine("Enter Employee's Salary");
+    float salary;
+    do
+    {
+        salary = float.Parse(Console.ReadLine());
+        if (salary <= 0)
+            Console.WriteLine("Enter a valid Salary");
+    }
+    while (salary <= 0);
+    Console.WriteLine("Enter Employee's Hiring Date as dd-mm-yy");
+    string[] DateArr;
+    do
+    {
+        DateArr = Console.ReadLine().Split('-');
+        if(int.Parse(DateArr[0]) > 31 || int.Parse(DateArr[0]) < 1)
+            Console.WriteLine("Enter a valid day (1~31)");
+        else if(int.Parse(DateArr[1]) > 12 || int.Parse(DateArr[1]) < 1)
+            Console.WriteLine("Enter a valid month (1~12)");
+        else if (int.Parse(DateArr[2]) > 2023 || int.Parse(DateArr[2]) < 1900)
+            Console.WriteLine("Enter a valid year (1900~2023)");
+    } while (int.Parse(DateArr[0]) > 31 || int.Parse(DateArr[0]) < 1 || int.Parse(DateArr[1]) > 12 || int.Parse(DateArr[1]) < 1 || int.Parse(DateArr[2]) > 2023 || int.Parse(DateArr[2]) < 1900);
+    Date hiringDate = new Date(int.Parse(DateArr[0]), int.Parse(DateArr[1]), int.Parse(DateArr[2]));
+    Console.WriteLine("Enter Employee's Gender (Male or Female)");
+    Gender Gen = (Gender) Enum.Parse(typeof(Gender), Console.ReadLine());
+    Emps[i] = new Employee(id,name,security,salary,hiringDate,Gen);
+}
+
+for (int i = 0; i < Emps.Length; i++)
+{
+    Emps[i].ShowInfo();
+}
