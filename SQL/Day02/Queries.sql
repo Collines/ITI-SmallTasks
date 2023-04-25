70f715646aa53e4388e9fdcdd42de16836e981e7
Mohamed Salah (MiDo) <mohammed.mido9719@gmail.com>
2023-04-25 11:58:23 +0200

    Added Day02 SQL Task Solution

---
+++ SQL/Day02/Queries.sql
@@ -0,0 +1,9 @@
+/* select * from Employee */
+/* select fname, lname, salary, dno from employee */
+/* select pname, plocation, dnum from project */
+/* select (fname+' '+lname) as 'Full name', 0.1*salary*12 as annual_commition from employee */
+/* select ssn, fname,lname  from employee where salary >1000; */
+/* select ssn, fname,lname, salary  from employee where salary*12 > 10000; */
+/* select fname, lname, salary  from employee where sex ='f'*/
+/* select dnum, dname from Departments where mgrssn=968574 */
+/* select pnumber, pname, Plocation from Project where dnum=10 */
